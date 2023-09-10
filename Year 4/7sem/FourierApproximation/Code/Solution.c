10af1c48d0a0845cfe8463f09f704ab323fd33ea
VsevolodZaostrovsky <zaos2013@mail.ru>
2023-09-10 18:06:59 +0300

    It doesnt work at all

--- Year 4/7sem/FourierApproximation/Code/Solution.c
+++ Year 4/7sem/FourierApproximation/Code/Solution.c
@@ -6,7 +6,6 @@
 # define M_PI           3.14159265358979323846
 
 // требует массив длины N+1, записывает в него сеть узлов, сгенерированных по правилам, описанным в репорте
-
 int GenerateNet(double *net, int N) 
 {
     double x_k = 0;
@@ -27,18 +26,7 @@ int GenerateNet(double *net, int N)
     return NET_GENERATED_WELL;
 }
 
-// применяет функцию u к массиву array длины len и записывает результат в ans
-
-void map(double (*u)(double), double * array, int len, double * ans)
-{
-    for(int m = 0; m < len; ++m)
-    {
-        ans[m] = u(array[m]);
-    }
-}
-
 // перемножает попарно и считает сумму произведений элементов массивов v1 и v2 с индекса start до индекса finish, не включая последний
-
 double SCProduct(int start, int finish, double * v1, double * v2) 
 {
     double ans = 0;
@@ -52,17 +40,48 @@ double SCProduct(int start, int finish, double * v1, double * v2)
 }
 
 // функции, вычисляющие и записывающие базисный вектор 
-
 double Phi(int m, int k, int N) 
 {
     double h = 1 / (N + 0.5);
-    return sin(M_PI * m * ( - h / 2 + k * h));
+    return sin( M_PI * m * ( - h / 2 + k * h) );
 }
 
-void printPhiTo(int m, int N, double * ph) 
+void WritePhiTo(int m, int N, double * ph) 
 {
     for(int k = 1; k < N; ++k) 
     {
         ph[k] = Phi(m, k, N);
     }
 }
+
+int WriteCNkTo(int N, double * cNks, double (*u)(double), double * netmemory, double * umemory, double * phimemory)
+{
+    if(GenerateNet(netmemory, N) == NET_GENERATION_ERROR) 
+    {
+        return NET_GENERATION_ERROR;
+    }
+
+    for(int m = 1; m < N; ++m)
+    {
+        umemory[m] = u(netmemory[m]);
+    }
+
+    for(int k = 1; k < N; ++k) 
+    {
+        WritePhiTo(k, N, phimemory);
+        cNks[k] = 2 * SCProduct(1, N, phimemory, umemory);
+    }
+
+    return 0;
+}
+
+double FourierCompute(double * coefs, int N, double x) 
+{
+    double ans = 0;
+    for(int m = 1; m < N; ++m)
+    {
+        ans += coefs[m] * sin( M_PI * m * x );
+    }
+    return ans;
+}
+
